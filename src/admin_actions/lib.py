447506b987a7cc57c3eca87bc9e20177fbed5b24
Kenneth Love <11908+kennethlove@users.noreply.github.com>
2025-11-26 09:40:36 -0800

    docs: Added more documentation to `lib.py`.
    * Expanded `lib.py` docstrings and comments
    * Improved a couple of type hints
    * Avoided a potential `AttributeError` involving `None`.

--- src/admin_actions/lib.py
+++ src/admin_actions/lib.py
@@ -1,14 +1,18 @@
+"""Provides an abstract base class for generating admin actions."""
+
 import abc
 from collections.abc import Callable
 from typing import Any, TypeAlias
 
 from django.contrib import messages
 from django.contrib.admin import ModelAdmin
-from django.db.models import QuerySet
+from django.db.models import Model, QuerySet
 from django.http import HttpRequest
 
-Condition: TypeAlias = Callable[[Any], bool]  # Condition to enable the action
-Function: TypeAlias = Callable[[Any], None]
+Condition: TypeAlias = Callable[
+    [Any], bool
+]  # Condition to enable the function for an item.
+Function: TypeAlias = Callable[[Any], None]  # Function to call for each item.
 
 
 class AdminActionBaseClass(abc.ABC):
@@ -21,8 +25,7 @@ class AdminActionBaseClass(abc.ABC):
         conditional_action = MyAdminAction(
             function=my_function,
             condition=lambda record: record.should_process(),
-            name="process_records",
-        )
+            name="process_records")
 
         def my_function(record_id):
             record = MyModel.objects.get(pk=record_id)
@@ -32,8 +35,7 @@ class AdminActionBaseClass(abc.ABC):
             actions = [conditional_action]
             model = MyModel
 
-    :param function: Required. Should be a callable that takes a single model instance's
-        primary key as an argument.
+    :param function: Required. Should be a callable that takes a single model instance as an argument.
     :param condition: Optional. If provided, it should be a callable that takes a model
         instance and returns a boolean indicating whether to queue the task for that record.
     :param name: Optional. If provided, it will be used as the action's name in the admin
@@ -42,27 +44,51 @@ class AdminActionBaseClass(abc.ABC):
 
     @abc.abstractmethod
     def handle_item(self, item):
-        """Handles a single item from the queryset."""
+        """Handles a single item from the queryset.
 
-    # noinspection PyProtectedMember
+        This method will be called for each item in the queryset that passes the condition. Any
+        subclass must implement this method to define how to process each item.
+
+        NOTE: This method _is not_ asynchronous or in another thread/process; large quantities of
+        work should be done in other ways or places, not solely in this method.
+
+        :param item: The model instance being process.
+        """
+
     def __call__(
-        self, modeladmin: ModelAdmin, request: HttpRequest, queryset: QuerySet
+        self, modeladmin: ModelAdmin, request: HttpRequest, queryset: QuerySet[Model]
     ) -> None:
-        """Admin action to call `self.function` for `queryset` records that pass `self.condition`."""
-        _count: int = 0
+        """Calls ``self.handle_item`` for each item in ``queryset`` that passes ``self.condition``.
+
+        :param modeladmin: The admin instance for the model being processed.
+        :param request: The current HTTP request object.
+        :param queryset: The queryset of records to process.
+        """
+        _count: int = 0  # Number of records successfully processed
 
         for record in queryset:
-            if not self.condition(record):
+            if not self.condition(
+                record
+            ):  # Skip any records that don't meet the condition
                 continue
-            self.handle_item(record)
-            _count += 1
+            self.handle_item(record)  # Apply the function to the record
+            _count += 1  # Increment the counter, record was successfully processed
 
-        if _count:
-            model_name = queryset.model._meta.verbose_name_plural.title()
+        if _count:  # If any records were processed, notify the user
+            # Get the appropriate plural model name, or a reasonable fallback
+            model_name = (
+                queryset.model._meta.verbose_name_plural
+                or queryset.model.__name__ + "s"
+            )
             if _count == 1:
-                model_name = queryset.model._meta.verbose_name.title()
+                # Get the appropriate singular model name, or a reasonable fallback
+                model_name = (
+                    queryset.model._meta.verbose_name or queryset.model.__name__
+                )
 
-            modeladmin.message_user(
+            model_name = model_name.title()
+
+            modeladmin.message_user(  # Add a success message for the user
                 request,
                 f"Called {self.__name__} for {_count} {model_name}.",
                 messages.SUCCESS,
@@ -75,7 +101,13 @@ class AdminActionBaseClass(abc.ABC):
         condition: Condition | None = None,
         name: str | None = None,
     ) -> None:
-        """Initializes the action with a function and optional condition."""
+        """Initializes the action with a function and optional condition.
+
+        :param function: Required. Should be a callable that takes a single model instance.
+        :param condition: Optional. If provided, it should be a callable that takes a model instance and returns a
+            Boolean indicating whether to queue the task for that record.
+        :param name: Optional. If provided, will be used as the action's name in the admin interface.
+        """
 
         if condition is not None:
             if isinstance(condition, Callable):  # Cannot call a non-callable condition
